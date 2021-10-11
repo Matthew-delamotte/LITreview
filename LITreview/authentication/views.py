b41e4de557a08dd9031d5bb4cde07acdf025eb8f
Matthew-delamotte <82178843+Matthew-delamotte@users.noreply.github.com>
2021-10-12 01:49:07 +0200

    update 0.2 - login view create
    Creating forms.py
    Creating templates dir (all app)
    Creating login_page view
    Setting login page gabarit and function

--- LITreview/authentication/views.py
+++ LITreview/authentication/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate 
+
+from . import forms
 
 # Create your views here.
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['passeword'],
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Bonjour, {user.username}! Vous êtes connecté.'
+            else:
+                message = 'Identifiant invalide.'
+    return render(request, 'authentification/login.html', context={'form': form, 'message': message})
