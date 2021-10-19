b374e671631934bc01b68a4fb8fbf83d3e6a52f9
Matthew-delamotte <82178843+Matthew-delamotte@users.noreply.github.com>
2021-10-19 15:09:33 +0200

    Update 0.3.01

--- LITreview/flux/views.py
+++ LITreview/flux/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 @login_required
 def home(request):
     return render(request, 'flux/home.html')
+
+# @login_required
+# def blog_and_photo_upload(request):
+#     blog_form = forms.BlogForm()
+#     if request.method == "POST":
+#         context = {
+#             'blog_form': blog_form,
+#         }
+#     return render(request, 'blog/create_blog_post.html', context=context)
